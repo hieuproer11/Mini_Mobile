69ee7b0ccd6118a1ebe9feae8e7e1c1f34272561
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2024-12-09 14:58:32 +0100

    push (Acceuil & Select Activities)

--- app/src/main/java/com/example/p2025_2sio_mobile_tran_beriot/SelectActivity.java
+++ app/src/main/java/com/example/p2025_2sio_mobile_tran_beriot/SelectActivity.java
@@ -1,12 +1,8 @@
 package com.example.p2025_2sio_mobile_tran_beriot;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,19 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
-public class AccueilActivity extends AppCompatActivity {
-
+public class SelectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_accueil);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_selection);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.selectPage), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    private Button createButton;
+    private ListView listViewJeux;
 
-    private Button selectButton;
+    private ArrayList<JeuDeSociete> mesJeux = new ArrayList<JeuDeSociete>();
+
+    private void init()
+    {
+        mesJeux.add(new JeuDeSociete("Monopoly","Lorem Ipsum, bla bla bla","Hasbro",15.65,"photo",1));
+        mesJeux.add(new JeuDeSociete("TTMC","Lorem Ipsum, bla bla bla","A",20,"photo",2500));
+        mesJeux.add(new JeuDeSociete("AAAAAA","Lorem Ipsum, bla bla bla","AAAAAA",15.65,"photo",0));
+    }
 }
