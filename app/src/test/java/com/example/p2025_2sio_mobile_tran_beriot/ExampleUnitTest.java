fb01c90a7c89bc5f9847b0ca1754645405f4087b
hieuproer11 <84191432+hieuproer11@users.noreply.github.com>
2024-12-09 13:36:29 +0100

    mobile 1

---
+++ app/src/test/java/com/example/p2025_2sio_mobile_tran_beriot/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.p2025_2sio_mobile_tran_beriot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
